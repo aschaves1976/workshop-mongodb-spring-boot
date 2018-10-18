69430205d46cfc6cfc59adf8dfdeeb17395aab8e
aschaves1976 <roots17@gmail.com>
2018-10-18 16:28:36 -0300

    Deleção de usuário com DELETE

--- src/main/java/br/com/aschaves/workshopmongo/resources/UserResource.java
+++ src/main/java/br/com/aschaves/workshopmongo/resources/UserResource.java
@@ -54,6 +54,15 @@ public class UserResource {
 		// boa prática
 		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/id").buildAndExpand(obj.getId()).toUri();
 		return ResponseEntity.created(uri).build();
+	}
+
+	@RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
+	public ResponseEntity<Void> delete(@PathVariable String id) {
+
+		service.delete(id);
+
+		return ResponseEntity.noContent().build();
 
 	}
+
 }
